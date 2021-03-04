80188b7245ae29f8167c9ad2099fabd953c39f37
sinanyuce <sinan.yuece@gmail.com>
2021-03-04 20:22:02 +0100

    subscribing to the store and select certain data to display

--- src/app/book-monkey/iteration-7/i18n/books/book-list/book-list.component.ts
+++ src/app/book-monkey/iteration-7/i18n/books/book-list/book-list.component.ts
@@ -3,8 +3,9 @@ import { Observable } from 'rxjs';
 
 import { Book } from '../../shared/book';
 import { BookStoreService } from '../../shared/book-store.service';
-import {Store} from '@ngrx/store';
+import {select, Store} from '@ngrx/store';
 import {loadBooks} from '../store/book.actions';
+import {selectAllBooks, selectBooksLoading} from '../store/book.selectors';
 
 @Component({
   selector: 'bm-book-list',
@@ -12,7 +13,12 @@ import {loadBooks} from '../store/book.actions';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-  books$: Observable<Book[]>;
+  books$: Observable<Book[]> = this.store.pipe(
+    select(selectAllBooks)
+  );
+  loading$ = this.store.pipe(
+    select(selectBooksLoading)
+  );
 
   constructor(private store: Store) { }
 
