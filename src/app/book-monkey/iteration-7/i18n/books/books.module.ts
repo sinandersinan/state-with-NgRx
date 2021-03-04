bc8124b1a1ab922bfe8662a892b597c0bce929e5
sinanyuce <sinan.yuece@gmail.com>
2021-03-04 19:32:32 +0100

    registering store

--- src/app/book-monkey/iteration-7/i18n/books/books.module.ts
+++ src/app/book-monkey/iteration-7/i18n/books/books.module.ts
@@ -8,11 +8,17 @@ import { BookListItemComponent } from './book-list-item/book-list-item.component
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { IsbnPipe } from './shared/isbn.pipe';
 import { ZoomDirective } from './shared/zoom.directive';
+import { StoreModule } from '@ngrx/store';
+import * as fromBook from './store/book.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './store/book.effects';
 
 @NgModule({
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    StoreModule.forFeature(fromBook.bookFeatureKey, fromBook.reducer),
+    EffectsModule.forFeature([BookEffects])
   ],
   declarations: [
     BookListComponent,
